17cebb11978700a316eb5255ad02e4e31883cc3b
JOAO PEDRO MOREIRA MARTINS DE SOUSA <joao.sousa@fasec.edu.br>
2025-04-14 19:21:23 -0300

    Add files via upload

---
+++ Exercicio1.py
@@ -0,0 +1,10 @@
+## Peça ao usuário para digitar um número inteiro positivo.
+## Aluno: João Pedro Moreira
+
+
+## Função
+num = int (input("Digite um número inteiro positivo: "))
+if num <= 0:
+    print("Incorreto")
+else:
+    print("Correto")
